b2fc7b1fb92b85543875893ef3cfa26dcae6239d
akilkumar <akilkumar@LAPTOP-KFT6O2IH>
2022-06-22 16:52:33 +0530

    Roopa

---
+++ Thread_Project/src/Thread/thread.java
@@ -0,0 +1,17 @@
+package Thread;
+
+public class thread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread t=Thread.currentThread();
+		System.out.println(t);
+		System.out.println("the thread name is "+t.getName());
+        t .setName("problem");
+        System.out.println("new thread name is="+t.getName());
+      		
+		
+
+	}
+
+}
